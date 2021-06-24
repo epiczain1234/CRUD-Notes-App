21d9353b6f1552a47dc144725df9731d16c51ac0
gdq5 <gdq5@M-C02C11GWLVDL.nordstrom.net>
2021-06-24 13:56:05 -0700

    Added error handling on signup page

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", text="Test", boolean=False)
 
 @auth.route('/logout')
 def logout():
     return "logged out"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords must match', category='error')
+        elif len(password1) < 7:
+            flash('Passwords need to be at contain at least 7 characters.', category='error')
+        else:
+            flash('Account Created', category='success')
     return render_template("signup.html")
